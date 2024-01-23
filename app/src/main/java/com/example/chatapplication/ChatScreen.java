7e80a5846acc566be2e7458fce215db7d64e921e
Prabhash021 <shankar.prabhash.21@gmail.com>
2024-01-23 18:37:36 +0530

    working on chat feature

--- app/src/main/java/com/example/chatapplication/ChatScreen.java
+++ app/src/main/java/com/example/chatapplication/ChatScreen.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,8 +17,9 @@ public class ChatScreen extends AppCompatActivity {
 
     TextView chatUserName;
     ListView chat;
-
-
+    EditText msg;
+    Button send;
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +27,29 @@ public class ChatScreen extends AppCompatActivity {
         setContentView(R.layout.activity_chat_screen);
 
         chatUserName = findViewById(R.id.chatUser);
+        msg = findViewById(R.id.chatMsg);
+        send = findViewById(R.id.sendMsg);
+        chat = findViewById(R.id.chatLV);
 
         chatUserName.setText(getChatUserName());
 
+        arrayList = new ArrayList<>();
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
 
+        chat.setAdapter(adapter);
 
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newMsg = msg.getText().toString();
+                if(!newMsg.isEmpty()){
+                    arrayList.add(newMsg);
+                    adapter.notifyDataSetChanged();
+                    msg.setText("");
+                }
+            }
+        });
     }
 
     private String getChatUserName() {
